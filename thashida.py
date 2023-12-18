7fadd5eb3f19a213da71bbd6a27d1b1632b002b4
thashidasamjad <154225625+thashidasamjad@users.noreply.github.com>
2023-12-18 02:53:12 -0800

    Add files via upload

---
+++ thashida.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum():
+    a=10
+    b=20
+    print(a+b)
+
+
+# In[2]:
+
+
+sum()
+
+
+# In[3]:
+
+
+def sub(a,b):
+    x=a
+    y=b
+    c=x-y
+    return c
+
+
+# In[4]:
+
+
+z=sub(30,10)
+print(z)
+
+
+# In[7]:
+
+
+a=int(input(" enter a number"))
+b=20
+if a>b:
+    print(" A is greater than B")
+else:
+    print("B is greater than A")
+
+
+# In[21]:
+
+
+a=int(input(" enter a number"))
+b=int(input(" enter a number"))
+z=sub(a,b):
+    print(sub)
+
+
+# In[14]:
+
+
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+
+
+# In[20]:
+
+
+div(10,7)
+
+
+# In[24]:
+
+
+a=("abc","def","ghi")
+len(a)
+
+
+# In[ ]:
+
+
+
+
